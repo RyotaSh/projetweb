08ccfab2fa76a433024ce39e88a9a89a7a17e3c2
Cheng <cervelexe@hotmail.fr>
2024-06-01 20:32:34 +0200

    pageclassefini

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,26 @@
-var counter = 1;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter ++;
-    if (counter>5){
-        counter=1;
-    }
-},5000);
+var input = document.getElementById("myInput");
+
+// image a trier
+var images = document.querySelectorAll(".descriperso");
+
+// detect les entree de l'utilisateur
+input.addEventListener("keyup", function() {
+    // recupere les entree et met en minuscule
+    var filter = input.value.toLowerCase();
+
+    
+    images.forEach(function(image) {
+        // recupere le texte de l'alt
+        var altText = image.getAttribute("alt").toLowerCase();
+        // compare l'entree et l'alt et affiche l'image si identique
+        if (altText.indexOf(filter) > -1) {
+            image.parentElement.style.display = "";
+        } else {
+            image.parentElement.style.display = "none";
+        }
+    });
+});
+
+function openPopup(imageSrc) {
+  window.open(imageSrc, "popup", "width=400,height=400");
+}
